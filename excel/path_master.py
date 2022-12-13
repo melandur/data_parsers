e923013aebd67dbfa218c118215db71f3d3b5268
Sebastian Balzer <sebastian.balzer@gmx.ch>
2022-12-13 09:05:56 +0100

    dev branch initial commit - added path suffix for testing

--- excel/path_master.py
+++ excel/path_master.py
@@ -1,20 +1,22 @@
 import os
 
-DATA_PATH = '/home/melandur/Data/Myocarditis/csv/train/'
+DATA_PATH = '/home/sebalzer/Documents/Mike_init/csv/train'
 
 # Prepare data
 RAW_PATH = os.path.join(DATA_PATH, '0_raw')
-EXTRACTED_PATH = os.path.join(DATA_PATH, '1_extracted')
-CASE_WISE_PATH = os.path.join(DATA_PATH, '2_case_wise')
-CLEANED_PATH = os.path.join(DATA_PATH, '3_cleaned')
-CHECKED_PATH = os.path.join(DATA_PATH, '4_checked')
-TABLE_WISE_PATH = os.path.join(DATA_PATH, '5_table_wise')
+# Path suffix for testing purposes
+SUFFIX = '_experimental'
+EXTRACTED_PATH = os.path.join(DATA_PATH, '1_extracted' + SUFFIX)
+CASE_WISE_PATH = os.path.join(DATA_PATH, '2_case_wise' + SUFFIX)
+CLEANED_PATH = os.path.join(DATA_PATH, '3_cleaned' + SUFFIX)
+CHECKED_PATH = os.path.join(DATA_PATH, '4_checked' + SUFFIX)
+TABLE_WISE_PATH = os.path.join(DATA_PATH, '5_table_wise' + SUFFIX)
 
 # Refine data
-CONDENSED_PATH = os.path.join(DATA_PATH, '6_condensed')
-MERGED_PATH = os.path.join(DATA_PATH, '7_merged')
-AHA_SEGMENT_PATH = os.path.join(DATA_PATH, '8_aha_segment')
+CONDENSED_PATH = os.path.join(DATA_PATH, '6_condensed' + SUFFIX)
+MERGED_PATH = os.path.join(DATA_PATH, '7_merged' + SUFFIX)
+AHA_SEGMENT_PATH = os.path.join(DATA_PATH, '8_aha_segment' + SUFFIX)
 
 # Analyze data
-ANALYZE_PATH = os.path.join(DATA_PATH, '16_analyze')
-TMP_PATH = os.path.join(DATA_PATH, '17_tmp')
+ANALYZE_PATH = os.path.join(DATA_PATH, '16_analyze' + SUFFIX)
+TMP_PATH = os.path.join(DATA_PATH, '17_tmp' + SUFFIX)
