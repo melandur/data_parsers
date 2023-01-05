a10bc9d236a21ba6cc239c09e27808d000317958
Sebastian Balzer <sebastian.balzer@gmx.ch>
2023-01-05 14:25:38 +0100

    (idea) col_counter
    benefit/loss of pre-initialization unclear

--- excel/analysis/utils/extract_data.py
+++ excel/analysis/utils/extract_data.py
@@ -22,44 +22,70 @@ class ExtractData:
         self.peak_values = peak_values
         self.relevant = []
         self.table_name = None
-        self.tables = {}
 
     def __call__(self) -> pd.DataFrame:
         # Identify relevant tables w.r.t. input parameters
-        self.identify_tables()
+        ncols = self.identify_tables()
+        # Pre-initialize DataFrame
+        # TODO: find a way to pre-define columns
+        tables = pd.DataFrame(index=subjects, columns=ncols)
         # Parse source directory to read in relevant tables
         subjects = os.listdir(self.src)
         for subject in subjects:
             for table in self.loop_files(subject):
                 if self.peak_values:
                     table = self.remove_time(table)
-                    self.extract_peak_values(table, subject)
+                    tables.iloc[subject, :] = self.extract_peak_values(table, subject)
                 else:
                     logger.error('peak_values=False is not implemented yet.')
 
-        tables = pd.DataFrame(self.tables)
         logger.debug(tables)
+        return tables
 
-    def identify_tables(self) -> None:
+    def identify_tables(self) -> int:
+        col_counter = 0
         for locality in self.localities:
             for dim in self.dims:
-                for axis in self.axes:
-                    for orientation in self.orientations:
-                        for metric in self.metrics:
+                for metric in self.metrics:
+                    # ROI analysis -> 4 cols per (dim, metric) combination
+                    if locality == 'roi':
+                        col_counter += 4
+                    # AHA analysis -> TODO
+                    elif locality == 'aha':
+                        logger.error('AHA analysis not implemented yet.')
+                    for axis in self.axes:
+                        for orientation in self.orientations:
+                            # Skip impossible combinations
+                            if axis == 'short_axis' and orientation == 'longit' or \
+                                axis == 'long_axis' and orientation == 'circumf':
+                                continue
+
                             self.relevant.append(
                                 f'{locality}_{dim}_{axis}_{orientation}_{metric}')
+
+    # def predefine_ncols(self) -> int:
+    #     """Predefine the number of columns of the final dataframe
+
+    #     Returns:
+    #         int: number of columns
+    #     """
+    #     count = 0
+    #     for dim in self.dims:
+    #         # ROI analysis
+    #         if any(f'roi_{dim}' in table_name for table_name in self.relevant):
+    #             count += 4
         
     def loop_files(self, subject) -> pd.DataFrame:
-            for root, _, files in os.walk(os.path.join(self.src, subject)):            
-                for file in files:
-                    # Consider only relevant tables
-                    for table_name in self.relevant:
-                        if file.endswith('.xlsx') and table_name in file:
-                            logger.info(f'Relevant table {table_name} found.')
-                            self.table_name = table_name
-                            file_path = os.path.join(root, file)
-                            table = pd.read_excel(file_path)
-                            yield table
+        for root, _, files in os.walk(os.path.join(self.src, subject)):            
+            for file in files:
+                # Consider only relevant tables
+                for table_name in self.relevant:
+                    if file.endswith('.xlsx') and table_name in file:
+                        logger.info(f'Relevant table {table_name} found.')
+                        self.table_name = table_name
+                        file_path = os.path.join(root, file)
+                        table = pd.read_excel(file_path)
+                        yield table
 
     def remove_time(self, table) -> None:
         return table[table.columns.drop(list(table.filter(regex='time')))]
@@ -77,6 +103,6 @@ class ExtractData:
             peak = table.iloc[:, info_cols:].max(axis=1)
 
         # Merge info cols with peak values
-        # table = pd.concat([table.iloc[:, :info_cols], peak], axis=1)
-        # table.rename(columns={0: f'peak_{self.table_name}'}, inplace=True)
-        self.tables[subject] = peak
+        table = pd.concat([table.iloc[:, :info_cols], peak], axis=1)
+        table.rename(columns={0: f'peak_{self.table_name}'}, inplace=True)
+        self.tables[subject] = table
